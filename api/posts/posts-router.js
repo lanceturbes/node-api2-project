aa14c4ac73360312f0d9e99f475c0a2f72618c13
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-26 11:50:37 -0700

    crafted [GET] /api/posts/:id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,6 +18,24 @@ router.get("/", async (req, res) => {
     });
   }
 });
+// [GET] /api/posts/:id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be retrieved",
+    });
+  }
+});
 
 // Exports
 module.exports = router;

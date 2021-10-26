7e755b4a16f94e76a9ff3907d75639b40e9ed049
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-26 12:40:39 -0700

    crafted [PUT] /api/posts/:id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -54,6 +54,32 @@ router.post("/", async (req, res) => {
     });
   }
 });
+// [PUT] /api/posts/:id
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, contents } = req.body;
+    const post = await Post.findById(id);
+    if (post) {
+      if (title && contents) {
+        await Post.update(id, { title, contents });
+        res.status(200).json({ id: Number(id), title, contents });
+      } else {
+        res.status(400).json({
+          message: "Please provide title and contents for the post",
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be modified",
+    });
+  }
+});
 
 // Exports
 module.exports = router;
